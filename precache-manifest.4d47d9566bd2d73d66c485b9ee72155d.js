b8a47454c3ac6eb55c28c091383564214a18f998
khashbat <khashbatk17@gmail.com>
2022-11-04 23:04:52 +0800

    Updates

---
+++ precache-manifest.4d47d9566bd2d73d66c485b9ee72155d.js
@@ -0,0 +1,442 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2216340a096cdf1f90",
+    "url": "/KarutoGame/static/css/main.732dd7f2.chunk.css"
+  },
+  {
+    "revision": "6c2216340a096cdf1f90",
+    "url": "/KarutoGame/static/js/main.6c221634.chunk.js"
+  },
+  {
+    "revision": "3dd370925ab85f2a7620",
+    "url": "/KarutoGame/static/js/1.3dd37092.chunk.js"
+  },
+  {
+    "revision": "a2fbf2c381bd50775d45",
+    "url": "/KarutoGame/static/css/2.a98406ff.chunk.css"
+  },
+  {
+    "revision": "a2fbf2c381bd50775d45",
+    "url": "/KarutoGame/static/js/2.a2fbf2c3.chunk.js"
+  },
+  {
+    "revision": "798e8ef9dca14b798edb",
+    "url": "/KarutoGame/static/js/runtime~main.798e8ef9.js"
+  },
+  {
+    "revision": "2fc1adda9deeaefcc3bd53b37bc77fc5",
+    "url": "/KarutoGame/static/media/shinobu.2fc1adda.png"
+  },
+  {
+    "revision": "ce9a81ac3daa79cdb938b350e1cc0c98",
+    "url": "/KarutoGame/static/media/karutaImage.ce9a81ac.webp"
+  },
+  {
+    "revision": "dd03429d5eec03f282326c314a770fd5",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (0).dd03429d.MP3"
+  },
+  {
+    "revision": "1e8211fa8c784d8264eedf80877670f6",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (1).1e8211fa.MP3"
+  },
+  {
+    "revision": "4cf3156292cfc6530a3660f04f5da3f7",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (10).4cf31562.MP3"
+  },
+  {
+    "revision": "0a79e6109e0ea2fd8813f97bec8a3d4d",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (100).0a79e610.MP3"
+  },
+  {
+    "revision": "794ec533b42b6332e298ed812fdb6b5f",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (11).794ec533.MP3"
+  },
+  {
+    "revision": "586107b43488b6637b31b6cf92a666f6",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (12).586107b4.MP3"
+  },
+  {
+    "revision": "ec77125b4aa7ba6dacc260456811d459",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (13).ec77125b.MP3"
+  },
+  {
+    "revision": "ae3fe0bf3f59b352884d65eac1b1d081",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (14).ae3fe0bf.MP3"
+  },
+  {
+    "revision": "392197899ad923ff9fc239d08a189404",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (15).39219789.MP3"
+  },
+  {
+    "revision": "b10ce07aec97270f9c2a05f76afa22d0",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (16).b10ce07a.MP3"
+  },
+  {
+    "revision": "fb7adf15b3a035cdcd1cc0981768b01f",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (17).fb7adf15.MP3"
+  },
+  {
+    "revision": "88d4c4c5f52558fb6f24a6ffd0d91d48",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (18).88d4c4c5.MP3"
+  },
+  {
+    "revision": "7c289b56771493c4911f64d1b36de240",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (19).7c289b56.MP3"
+  },
+  {
+    "revision": "2c6f1d1ead3befa09e9d9cf8b270afac",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (2).2c6f1d1e.MP3"
+  },
+  {
+    "revision": "773ea3717a94dd33b0fd1e7d1b2f1600",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (20).773ea371.MP3"
+  },
+  {
+    "revision": "c59c8c7f1ffd4a3d0bf798294ed5bf7c",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (21).c59c8c7f.MP3"
+  },
+  {
+    "revision": "b6af7ed0e9f2b9463a17de0218e1975e",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (22).b6af7ed0.MP3"
+  },
+  {
+    "revision": "2bead972f6f64fffbefe48987e76bee5",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (23).2bead972.MP3"
+  },
+  {
+    "revision": "0b0ac68ee729d932353692fc982c93c5",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (24).0b0ac68e.MP3"
+  },
+  {
+    "revision": "10307d2d880f624b3f47fc2bea8403a7",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (25).10307d2d.MP3"
+  },
+  {
+    "revision": "2fd6c39260cf238531afb8b9cac6e7d0",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (26).2fd6c392.MP3"
+  },
+  {
+    "revision": "7c15f26d0ff9dfef21298332cc0812c0",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (27).7c15f26d.MP3"
+  },
+  {
+    "revision": "33a799031eb389b1de4bcdfce6a3af70",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (28).33a79903.MP3"
+  },
+  {
+    "revision": "46e43b118d506d08948f34dc09e65b36",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (29).46e43b11.MP3"
+  },
+  {
+    "revision": "d3ce5530a1511b7838150d4da268e715",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (3).d3ce5530.MP3"
+  },
+  {
+    "revision": "416f36d4cc0e13c2ef82bfc8f228af10",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (30).416f36d4.MP3"
+  },
+  {
+    "revision": "9cf76b6e3a6ecee96921ccbcce7d1bde",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (31).9cf76b6e.MP3"
+  },
+  {
+    "revision": "3569010a88082191935d261a55e49af3",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (32).3569010a.MP3"
+  },
+  {
+    "revision": "d6f88905bb53c68a7029e2cea0a66476",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (33).d6f88905.MP3"
+  },
+  {
+    "revision": "27c7909416c81a598950517242c95db7",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (34).27c79094.MP3"
+  },
+  {
+    "revision": "813d523f3592fc7731a0d7db2d8d0178",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (35).813d523f.MP3"
+  },
+  {
+    "revision": "f5e6061dc89f5426a641d0863cd40d6d",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (36).f5e6061d.MP3"
+  },
+  {
+    "revision": "7aadd683fa276e2b81ce51b9104156e6",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (37).7aadd683.MP3"
+  },
+  {
+    "revision": "64569274c6fb4c2db2feff1616b85b37",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (38).64569274.MP3"
+  },
+  {
+    "revision": "06efe646412f4df5bfde51f20c529cd8",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (39).06efe646.MP3"
+  },
+  {
+    "revision": "8bc21071ea3c3379cf5bc1766fb2e0c3",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (4).8bc21071.MP3"
+  },
+  {
+    "revision": "a182d1a107de94993e8ac5c2c8b56e9a",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (40).a182d1a1.MP3"
+  },
+  {
+    "revision": "7349985bf2824e4e5a8285e1fde211a2",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (41).7349985b.MP3"
+  },
+  {
+    "revision": "b35f71687596e7efd02f0aabfc60b800",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (42).b35f7168.MP3"
+  },
+  {
+    "revision": "e4b105643e170741613570545aba5fa2",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (43).e4b10564.MP3"
+  },
+  {
+    "revision": "3066762d3038585ea6ab0bf465eff92a",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (44).3066762d.MP3"
+  },
+  {
+    "revision": "6ab6819b2b7dc06490599db34c9d29cd",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (45).6ab6819b.MP3"
+  },
+  {
+    "revision": "dc3e45ad09a073b76383b992f46d9c95",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (46).dc3e45ad.MP3"
+  },
+  {
+    "revision": "26ef34b8049f046bc6adca2f8a86a0a4",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (47).26ef34b8.MP3"
+  },
+  {
+    "revision": "07fd22755a94b8c926ceacaf2206a3be",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (48).07fd2275.MP3"
+  },
+  {
+    "revision": "85988aa348d298b4a9fe1ac8c33871df",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (49).85988aa3.MP3"
+  },
+  {
+    "revision": "a03cc894102282e3862a20f10b718709",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (5).a03cc894.MP3"
+  },
+  {
+    "revision": "185228c05df9c55e08fbaf13044bba83",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (50).185228c0.MP3"
+  },
+  {
+    "revision": "8c30d2329315f814a2b5203603a554f8",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (51).8c30d232.MP3"
+  },
+  {
+    "revision": "2889096017ff8fe4bfe9069e377b3248",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (52).28890960.MP3"
+  },
+  {
+    "revision": "0d98d1485c89e39f8b525e6713934cc6",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (53).0d98d148.MP3"
+  },
+  {
+    "revision": "5b4e8fbb771b6dd65393109be3240a7e",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (54).5b4e8fbb.MP3"
+  },
+  {
+    "revision": "471b70044292825278ac59c0e40df319",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (55).471b7004.MP3"
+  },
+  {
+    "revision": "89c4ac50a5b3140bab0bf83b0b2332bd",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (56).89c4ac50.MP3"
+  },
+  {
+    "revision": "0fd8f0a4b5fbf453184a7360daab3bd7",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (57).0fd8f0a4.MP3"
+  },
+  {
+    "revision": "3e2594afd13aa3f2f99f1f747ffb9067",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (58).3e2594af.MP3"
+  },
+  {
+    "revision": "f7dbe6fc96cdd89b32aabca75f3194fb",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (59).f7dbe6fc.MP3"
+  },
+  {
+    "revision": "2df5eac46dda3c43bd9ee8d9d39dfeeb",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (6).2df5eac4.MP3"
+  },
+  {
+    "revision": "471fd6f24da072bad6f1d199e3bae4e4",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (60).471fd6f2.MP3"
+  },
+  {
+    "revision": "b5e8ee9b0caec09dcae2326ec4d101e6",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (61).b5e8ee9b.MP3"
+  },
+  {
+    "revision": "e88ab7d086c55e1be3e8f22e729bae9f",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (62).e88ab7d0.MP3"
+  },
+  {
+    "revision": "82ed0eed83baebf904b54a0063e9a011",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (63).82ed0eed.MP3"
+  },
+  {
+    "revision": "521b474817485c6245e736ec0b59c281",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (64).521b4748.MP3"
+  },
+  {
+    "revision": "5e08355f93de5dd69172c95f60220361",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (65).5e08355f.MP3"
+  },
+  {
+    "revision": "0159bafaf83c90bfa797fe6ca17a18a1",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (66).0159bafa.MP3"
+  },
+  {
+    "revision": "8182f19e65f6104b490d37ca8d985913",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (67).8182f19e.MP3"
+  },
+  {
+    "revision": "3b81a1388c7c2fdd48834e59f5cbadba",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (68).3b81a138.MP3"
+  },
+  {
+    "revision": "9be66bc1f36cdc7027d2c4473fca7e43",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (69).9be66bc1.MP3"
+  },
+  {
+    "revision": "f0a5f8377e8b1f7ded74123c5c584a42",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (7).f0a5f837.MP3"
+  },
+  {
+    "revision": "c5587d8841e12892d668b0b62ce6fd9b",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (70).c5587d88.MP3"
+  },
+  {
+    "revision": "67830edab045d2f79504fee156f55ded",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (71).67830eda.MP3"
+  },
+  {
+    "revision": "75e5897250085916f49bb96ca95f4d06",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (72).75e58972.MP3"
+  },
+  {
+    "revision": "ad7dd050904128f1fc4e31ae6750f9ef",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (73).ad7dd050.MP3"
+  },
+  {
+    "revision": "7a2b7315e270294ec27784f2d8e8aa7f",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (74).7a2b7315.MP3"
+  },
+  {
+    "revision": "b9649ec2fd4b54030607bac01f951461",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (75).b9649ec2.MP3"
+  },
+  {
+    "revision": "38970f138662ffae1bfa0202ebc962a8",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (76).38970f13.MP3"
+  },
+  {
+    "revision": "c201fa8ef1a5fcb8767c9e1cfe05b479",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (77).c201fa8e.MP3"
+  },
+  {
+    "revision": "4a23840146aa76bbeba2dcfe384304de",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (78).4a238401.MP3"
+  },
+  {
+    "revision": "82105a11474eedf802049e70414604a9",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (79).82105a11.MP3"
+  },
+  {
+    "revision": "2c2af2020754529b18b13ff78fcffa87",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (8).2c2af202.MP3"
+  },
+  {
+    "revision": "f5c3c6deff52c49980319f29e6d9f721",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (80).f5c3c6de.MP3"
+  },
+  {
+    "revision": "adc2d3657bec56dc9477edd9d9c70a79",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (81).adc2d365.MP3"
+  },
+  {
+    "revision": "0ca23b9554391055349288ed18cc23a6",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (82).0ca23b95.MP3"
+  },
+  {
+    "revision": "ad581cebc78fa39cd61c20159ee6d48c",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (83).ad581ceb.MP3"
+  },
+  {
+    "revision": "c9f9a68ccbdf9d7c4fb3afc0b370f4ae",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (84).c9f9a68c.MP3"
+  },
+  {
+    "revision": "9c965efa10bf945dd35d879049051ed4",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (85).9c965efa.MP3"
+  },
+  {
+    "revision": "3d5c465eba4a08b05297c2ae8fcdea93",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (86).3d5c465e.MP3"
+  },
+  {
+    "revision": "0ced42bdb518fb079237f808d06d5015",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (87).0ced42bd.MP3"
+  },
+  {
+    "revision": "71ab97540359d091b6d696942b0a55c0",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (88).71ab9754.MP3"
+  },
+  {
+    "revision": "a245e408366fc8459ac862517512c98d",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (89).a245e408.MP3"
+  },
+  {
+    "revision": "254e6c4c37cab8faabf10e15e54297c4",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (9).254e6c4c.MP3"
+  },
+  {
+    "revision": "ac3941713feae5b5418e29015d234334",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (90).ac394171.MP3"
+  },
+  {
+    "revision": "dfeaaef284e987ddae9e2c9c9c3e3de7",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (91).dfeaaef2.MP3"
+  },
+  {
+    "revision": "a74a9d1aa799ab26599953633bc17700",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (92).a74a9d1a.MP3"
+  },
+  {
+    "revision": "a94e17edf845367327b7fee987a9a058",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (93).a94e17ed.MP3"
+  },
+  {
+    "revision": "6357ce750025c86d9233857f32d59d01",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (94).6357ce75.MP3"
+  },
+  {
+    "revision": "60a50d8bec5649f01e4453c2008db0c7",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (95).60a50d8b.MP3"
+  },
+  {
+    "revision": "b0eece1b871b542b38239bf30727d219",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (96).b0eece1b.MP3"
+  },
+  {
+    "revision": "159b7107aacf2c0d398f9212b9aa488b",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (97).159b7107.MP3"
+  },
+  {
+    "revision": "5feba8da653cd9632b46c88ab1da7723",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (98).5feba8da.MP3"
+  },
+  {
+    "revision": "3a97809ca5b1eadd9859b947150cd8a5",
+    "url": "/KarutoGame/static/media/Hyakunin_Isshu (99).3a97809c.MP3"
+  },
+  {
+    "revision": "bfff373f6da8dcc7245c961e185f3b0e",
+    "url": "/KarutoGame/index.html"
+  }
+];
